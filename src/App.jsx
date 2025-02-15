2d1fcaa079f103a8ce44cec4e0aaadd6230e83aa
arnoprincestonpan <pan.arno@gmail.com>
2025-02-15 05:32:03 -0800

    first commit - component first approach. Added InputBox.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <div className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+      style={{backgroundImage: `url('https://images.pexels.com/photos/6266283/pexels-photo-6266283.jpeg')`}}
+    >
+      <h1 className='bg-red-200'>Test for Tailwind</h1>
+    </div>
+  )
+}
+
+export default App
